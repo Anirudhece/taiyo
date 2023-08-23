68b6eb841451295cd37eecc64bbf1ef58bb1cab9
Anirudh <anirudhece2020@gmail.com>
2023-08-23 20:47:22 +0530

    map shown

--- src/scenes/chatsMaps/ChartsMaps.jsx
+++ src/scenes/chatsMaps/ChartsMaps.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Box } from "@mui/material";
 import LineGraph from "./LineGraph";
+import Map from "./Map";
+// import Leaflet from "../../components/Leaflet";
 
 function ChartsAndMaps() {
   return (
     <>
-      <LineGraph />
+      {/* <LineGraph /> */}
+      {/* <Leaflet /> */}
+      <Map />
     </>
   );
 }
--- src/scenes/chatsMaps/Map.jsx
+++ src/scenes/chatsMaps/Map.jsx
@@ -1,26 +1,27 @@
 import React from "react";
+import { Box } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css"; // without this it wont work
 
 function Map() {
-
   const fetchWorldwideData = async () => {
     const response = await fetch("https://disease.sh/v3/covid-19/all");
     return response.json();
   };
 
-  const fetchCountryData = async (countryCode) => {
-    const response = await fetch(
-      `https://disease.sh/v3/covid-19/countries/${countryCode}`
-    );
+  const fetchCountryData = async () => {
+    const response = await fetch(`https://disease.sh/v3/covid-19/countries/`);
     return response.json();
   };
 
   const { data: worldwideData, status: worldwideStatus } = useQuery(
-    "worldwideData",
+    ["worldwideData"],
     fetchWorldwideData
   );
   const { data: countryData, status: countryStatus } = useQuery(
-    "countryData",
-    () => fetchCountryData("your_country_code")
+    ["countryData"],
+    fetchCountryData
   );
 
   if (worldwideStatus === "loading" || countryStatus === "loading") {
@@ -30,19 +31,62 @@ function Map() {
   if (worldwideStatus === "error" || countryStatus === "error") {
     return <p>Error fetching data.</p>;
   }
+
+  //       <Marker position={position}>
+  //         <Popup>
+  //           A pretty CSS3 popup. <br /> Easily customizable.
+  //         </Popup>
+  //       </Marker>
+  const markers = countryData.map((country) => (
+    <Marker
+      key={country.country}
+      position={[country.countryInfo.lat, country.countryInfo.long]}
+    >
+      <Popup>
+        <div>
+          <h3>{country.country}</h3>
+          <p>Total Cases: {country.cases}</p>
+          <p>Recovered: {country.recovered}</p>
+          <p>Deaths: {country.deaths}</p>
+        </div>
+      </Popup>
+    </Marker>
+  ));
+
   return (
     <>
-      <div>
-        {/* A react leaflet map with markers that indicates the country name, total number
-of active, recovered cases and deaths in that particular country as a popup. */}
-        <h2>Worldwide COVID-19 Data</h2>
-        <pre>{JSON.stringify(worldwideData, null, 2)}</pre>
-
-        <h2>Country-Specific COVID-19 Data</h2>
-        <pre>{JSON.stringify(countryData, null, 2)}</pre>
-      </div>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Box>
+          <MapContainer
+            style={{ width: "700px", height: "400px" }}
+            center={[20, 80]}
+            zoom={4}
+            scrollWheelZoom={true}
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution="we love bear"
+            />
+            {markers}
+          </MapContainer>
+        </Box>
+      </Box>
     </>
   );
 }
 
 export default Map;
+
+// const position = [20, 80];
+
+//     <MapContainer center={position} zoom={4} scrollWheelZoom={true}>
+//       <TileLayer
+//         attribution="we love bear"
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       />
+//       <Marker position={position}>
+//         <Popup>
+//           A pretty CSS3 popup. <br /> Easily customizable.
+//         </Popup>
+//       </Marker>
+//     </MapContainer>
